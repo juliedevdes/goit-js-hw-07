e14f8aa68edc5712b40b5d0e611a5b55ee1de706
Julia Ohrimenko <juliajam2497@gmail.com>
2021-07-03 15:16:04 +0300

    Решено второе

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,11 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+
+ingredients.forEach(ingredient => {
+  const newLi = document.createElement('li');
+  newLi.textContent = ingredient;
+  document.querySelector('#ingredients').appendChild(newLi);
+});
+
+console.log(document.querySelector('#ingredients'));

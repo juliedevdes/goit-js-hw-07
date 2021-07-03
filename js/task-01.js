09b9dee9adb9e4daf97616005745386d5be18186
Julia Ohrimenko <juliajam2497@gmail.com>
2021-07-03 14:31:31 +0300

    Решено первое задание

--- js/task-01.js
+++ js/task-01.js
@@ -2,9 +2,30 @@
 
 // Посчитает и выведет в консоль количество категорий в ul#categories, то есть элементов li.item. Получится 'В списке 3 категории.'.
 
-// Для каждого элемента li.item в списке ul#categories, найдет и выведет в консоль текст заголовка элемента (тега h2) и количество элементов в категории (всех вложенных в него элементов li).
+// Для каждого элемента li.item в списке ul#categories, найдет и выведет в консоль текст заголовка элемента (тега h2) и
+//количество элементов в категории(всех вложенных в него элементов li).
 
 // Например для первой категории получится:
 
 // Категория: Животные
 // Количество элементов: 4
+
+const getNameAndQuantity = function () {
+  console.log(
+    `В списке ${
+      document.querySelectorAll('ul#categories .item').length
+    } категории`
+  );
+
+  const categories = document.querySelectorAll('.item');
+
+  for (const category of categories) {
+    console.log(
+      `Категория: ${category.querySelector('h2').textContent}, Количество: ${
+        category.querySelectorAll('ul li').length
+      }`
+    );
+  }
+};
+
+getNameAndQuantity();

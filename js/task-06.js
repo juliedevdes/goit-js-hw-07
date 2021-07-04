16f01a3ed328dca21d4fb57b252ca425e535af5c
Julia Ohrimenko <juliajam2497@gmail.com>
2021-07-04 15:25:22 +0300

    исправлено 6
    неправильное событие

--- js/task-06.js
+++ js/task-06.js
@@ -1,16 +1,16 @@
 const input = document.querySelector('#validation-input');
-input.addEventListener('focus', focusInputHandler);
+input.addEventListener('input', focusInputHandler);
 
 function focusInputHandler(event) {
   const inputLength = Number(event.currentTarget.dataset.length);
-  console.log(inputLength);
+  //console.log(inputLength);
 
   const inputValue = event.currentTarget.value.length;
-  console.log(inputValue);
+  //console.log(inputValue);
 
   input.className = 'invalid';
 
-  console.log(inputLength === inputValue);
+  //console.log(inputLength === inputValue);
   if (inputLength === inputValue) {
     input.className = 'valid';
   }
